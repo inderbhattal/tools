32edd065337e00a9d1ecd794a9078277b86820dc
Inder Bhattal <reachinderbhattal@gmail.com>
2022-06-19 20:29:16 -0500

    fix for linking libs

--- install_realvnc.sh
+++ install_realvnc.sh
@@ -6,16 +6,14 @@ wget https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-6.9.1-Linu
 echo "Installing RealVNC server"
 sudo dpkg -i VNC-Server-6.9.1-Linux-ARM64.deb
 
-cd /usr/lib/aarch64-linux-gnu
-
 echo "Linking libs now!!!"
-sudo ln libvcos.so /usr/lib/libvcos.so.0
-sudo ln libvchiq_arm.so /usr/lib/libvchiq_arm.so.0
-sudo ln libbcm_host.so /usr/lib/libbcm_host.so.0
-sudo ln libmmal.so /usr/lib/libmmal.so.0
-sudo ln libmmal_core.so /usr/lib/libmmal_core.so.0
-sudo ln libmmal_components.so /usr/lib/libmmal_components.so.0
-sudo ln libmmal_util.so /usr/lib/libmmal_util.so.0
-sudo ln libmmal_vc_client.so /usr/lib/libmmal_vc_client.so.0
-sudo ln libvcsm.so /usr/lib/libvcsm.so.0
-sudo ln libcontainers.so /usr/lib/libcontainers.so.0
+sudo ln /usr/lib/aarch64-linux-gnu/libvcos.so /usr/lib/libvcos.so.0
+sudo ln /usr/lib/aarch64-linux-gnu/libvchiq_arm.so /usr/lib/libvchiq_arm.so.0
+sudo ln /usr/lib/aarch64-linux-gnu/libbcm_host.so /usr/lib/libbcm_host.so.0
+sudo ln /usr/lib/aarch64-linux-gnu/libmmal.so /usr/lib/libmmal.so.0
+sudo ln /usr/lib/aarch64-linux-gnu/libmmal_core.so /usr/lib/libmmal_core.so.0
+sudo ln /usr/lib/aarch64-linux-gnu/libmmal_components.so /usr/lib/libmmal_components.so.0
+sudo ln /usr/lib/aarch64-linux-gnu/libmmal_util.so /usr/lib/libmmal_util.so.0
+sudo ln /usr/lib/aarch64-linux-gnu/libmmal_vc_client.so /usr/lib/libmmal_vc_client.so.0
+sudo ln /usr/lib/aarch64-linux-gnu/libvcsm.so /usr/lib/libvcsm.so.0
+sudo ln /usr/lib/aarch64-linux-gnu/libcontainers.so /usr/lib/libcontainers.so.0
