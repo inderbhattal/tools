d90202315b6b187724d68f3fe070e30310546516
Inder Bhattal <reachinderjeetbhattal@gmail.com>
2023-10-22 19:29:25 -0500

    add atom repo w/o use input

--- install_atom.sh
+++ install_atom.sh
@@ -5,6 +5,6 @@ sudo apt -y install software-properties-common apt-transport-https wget
 
 wget -q https://packagecloud.io/AtomEditor/atom/gpgkey -O- | sudo apt-key add -
 
-sudo add-apt-repository "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main"
+sudo add-apt-repository "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" -y
 
 sudo apt -y install atom
