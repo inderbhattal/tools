eae340ceac7a729fabd7bc09d71dd8b3fc605dd7
Inder Bhattal <reachinderjeetbhattal@gmail.com>
2023-10-22 19:29:25 -0500

    adding script to install teamviewer on arm

---
+++ install_teamviewer_arm.sh
@@ -0,0 +1,17 @@
+echo "Installing Teamviewer"
+
+sudo apt-get update && sudo apt-get upgrade
+
+echo "Adding armhf architecture"
+sudo dpkg --add-architecture armhf
+
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get -f install
+
+sudo apt-get -y install libxtst6:armhf
+
+echo "Downloading Teamviewer..."
+wget https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb
+
+echo "Running Installer..."
+sudo dpkg -i teamviewer-host_armhf.deb
