b9282eb65059a3747eb3b5f585fb3e18b380a826
Inder Bhattal <reachinderjeetbhattal@gmail.com>
2023-10-22 19:29:25 -0500

    adding useful scripts and jetbrains install ide script

--- src/install_jetbrain_ide.py
+++ src/install_jetbrain_ide.py
@@ -22,7 +22,10 @@ def get_ide_tar_paths():
             os.rename(old_file_path, new_file_path)
             file_paths.append(new_file_path)
 
-    return sorted(file_paths)
+    file_paths = sorted(file_paths)
+    logger.info(f"{file_paths}")
+
+    return file_paths
 
 
 def get_ide_folders():
@@ -46,10 +49,11 @@ def install_ide():
     ide_folders = get_ide_folders()
     ide_tar_file_paths = get_ide_tar_paths()
 
-    for ide_tar_file_path in ide_tar_file_paths:
+    for index, ide_tar_file_path in enumerate(ide_tar_file_paths):
         if "idea" in ide_tar_file_path:
             new_ide_tar_file_path = ide_tar_file_path.replace("ideaiu", "idea-iu")
             os.rename(ide_tar_file_path, new_ide_tar_file_path)
+            ide_tar_file_paths[index] = new_ide_tar_file_path
 
             break
 
@@ -69,13 +73,13 @@ def install_ide():
         ide_folder_path = ide_folders.get(ide_name)
         ide_properties_file_path = os.path.join(ide_folder_path, os.path.join("bin", "idea.properties"))
 
-        saved_ide_properties_file_path = os.path.join(ide_root_dir, "idea.properties")
-        if os.path.isfile(ide_properties_file_path):
-            with open(ide_properties_file_path) as file:
-                data = file.read()
-
-                with open(saved_ide_properties_file_path, "w") as new_file:
-                    new_file.write(data)
+        # saved_ide_properties_file_path = os.path.join(ide_root_dir, "idea.properties")
+        # if os.path.isfile(ide_properties_file_path):
+        #     with open(ide_properties_file_path) as file:
+        #         data = file.read()
+        #
+        #         with open(saved_ide_properties_file_path, "w") as new_file:
+        #             new_file.write(data)
 
         logger.info(f"Removing contents of: {ide_folder_path}")
         shutil.rmtree(ide_folder_path)
@@ -96,16 +100,16 @@ def install_ide():
         logger.info(f"Renaming old folder {new_folder_path_extracted} -> {new_folder_path}")
         os.rename(new_folder_path_extracted, new_folder_path)
 
-        logger.info(f"Renaming default file properties at: {ide_properties_file_path}")
-        os.rename(ide_properties_file_path, f"{ide_properties_file_path}.ori")
-
-        logger.info(f"Writing saved config to: {ide_properties_file_path}")
-        if os.path.isfile(saved_ide_properties_file_path):
-            with open(saved_ide_properties_file_path) as file:
-                data = file.read()
-
-                with open(ide_properties_file_path, "w") as new_file:
-                    new_file.write(data)
+        # logger.info(f"Renaming default file properties at: {ide_properties_file_path}")
+        # os.rename(ide_properties_file_path, f"{ide_properties_file_path}.ori")
+        #
+        # logger.info(f"Writing saved config to: {ide_properties_file_path}")
+        # if os.path.isfile(saved_ide_properties_file_path):
+        #     with open(saved_ide_properties_file_path) as file:
+        #         data = file.read()
+        #
+        #         with open(ide_properties_file_path, "w") as new_file:
+        #             new_file.write(data)
 
     clean_ide_install_files(ide_tar_file_paths)
 
