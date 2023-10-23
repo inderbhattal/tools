1218040ebe3804862b7269540a4e369766c8a4e1
Inder Bhattal <reachinderjeetbhattal@gmail.com>
2023-10-22 19:29:25 -0500

    use correct syntax for .deb file installation, cleanup installtion file

--- install_vscode.sh
+++ install_vscode.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 echo "Making vscode symlinks"
-sudo ln -s /mnt/SSD-1/vscode/.vscode/ ~/.vscode
+sudo ln -s /mnt/SSD-1/vscode/.vscode/ /home/inder/.vscode
 
 echo "Getting vscode deb install file"
 wget https://go.microsoft.com/fwlink/?LinkID=760868 -O code.deb
 
 echo "Installing VSCode"
-sudo apt install ./code.deb
+sudo dpkg -i code.deb
+
+sudo rm code.deb
