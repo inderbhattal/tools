dc9784d8bd74bba006df1b2473082e32c548bf16
Inder Bhattal <reachinderjeetbhattal@gmail.com>
2023-10-22 19:29:25 -0500

    links libs before running realvnc install script

--- install_realvnc.sh
+++ install_realvnc.sh
@@ -3,9 +3,6 @@ sudo apt update && sudo apt upgrade -y
 echo "Download RealVNC server"
 wget https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-6.9.1-Linux-ARM64.deb
 
-echo "Installing RealVNC server"
-sudo dpkg -i VNC-Server-6.9.1-Linux-ARM64.deb
-
 echo "Linking libs now!!!"
 sudo ln /usr/lib/aarch64-linux-gnu/libvcos.so /usr/lib/libvcos.so.0
 sudo ln /usr/lib/aarch64-linux-gnu/libvchiq_arm.so /usr/lib/libvchiq_arm.so.0
@@ -17,6 +14,9 @@ sudo ln /usr/lib/aarch64-linux-gnu/libmmal_util.so /usr/lib/libmmal_util.so.0
 sudo ln /usr/lib/aarch64-linux-gnu/libmmal_vc_client.so /usr/lib/libmmal_vc_client.so.0
 sudo ln /usr/lib/aarch64-linux-gnu/libvcsm.so /usr/lib/libvcsm.so.0
 sudo ln /usr/lib/aarch64-linux-gnu/libcontainers.so /usr/lib/libcontainers.so.0
+
+echo "Installing RealVNC server"
+sudo dpkg -i VNC-Server-6.9.1-Linux-ARM64.deb
 
 echo "Removing installer!"
 rm VNC-Server-6.9.1-Linux-ARM64.deb
