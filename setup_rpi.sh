6328bcad57ffab4d22d6d5c402ad3dd10b060c85
Inder Bhattal <reachinderjeetbhattal@gmail.com>
2023-10-22 19:29:25 -0500

    Adding impl. to install vlan and avahi-utils for RPI

--- setup_rpi.sh
+++ setup_rpi.sh
@@ -6,3 +6,10 @@ apt update && sudo apt upgrade -y
 
 echo "Installing build-essential packages"
 apt install -y build-essential
+
+echo "Installing VLAN"
+sh ./setup_vlan.sh
+
+echo "Installing avahi-utils"
+sudo apt-get install -y avahi-utils
+
