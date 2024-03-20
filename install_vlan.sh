f3c4dce3dbee7b8b357442a9bc8a801773a7348a
Inder Bhattal <reachinderjeetbhattal@gmail.com>
2024-03-20 17:46:01 -0500

    Adding install and setup vlans scripts

---
+++ install_vlan.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Installing VLAN package"
+
+sudo apt -y install vlan
+
+echo "Installing kernel modules"
+sudo apt -y install linux-modules-extra-raspi
--- setup_vlan.sh
+++ setup_vlan.sh
@@ -1,8 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "Installing VLAN package"
+echo "Setting up VLAN's"
 
-sudo apt -y install vlan
+sudo ip link add eth0.30 link eth0 type vlan id 30
+sudo dhclient eth0.30
 
-echo "Installing kernel modules"
-sudo apt -y install linux-modules-extra-raspi
+sudo ip link add eth0.32 link eth0 type vlan id 32
+sudo dhclient eth0.32
+
+sudo ip link add eth0.39 link eth0 type vlan id 39
+sudo dhclient eth0.39
+
+sudo ip link add eth0.40 link eth0 type vlan id 40
+sudo dhclient eth0.40
+
+echo "Restarting avahi"
+sudo service avahi-daemon restart
