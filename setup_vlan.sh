1e86fbaccd3aa3bd636f8b01c969d62d09e19fac
Inder Bhattal <reachinderjeetbhattal@gmail.com>
2025-10-30 22:07:37 -0500

    adding script to install vlan and example vlan service

--- setup_vlan.sh
+++ setup_vlan.sh
@@ -4,15 +4,19 @@ echo "Setting up VLAN's"
 
 sudo ip link add eth0.30 link eth0 type vlan id 30
 sudo dhclient eth0.30
+sudo ip link set dev eth0.30 up
 
 sudo ip link add eth0.32 link eth0 type vlan id 32
 sudo dhclient eth0.32
+sudo ip link set dev eth0.32 up
 
 sudo ip link add eth0.39 link eth0 type vlan id 39
 sudo dhclient eth0.39
+sudo ip link set dev eth0.39 up
 
 sudo ip link add eth0.40 link eth0 type vlan id 40
 sudo dhclient eth0.40
+sudo ip link set dev eth0.40 up
 
 echo "Restarting avahi"
 sudo service avahi-daemon restart
